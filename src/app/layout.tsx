6742d6854844c85e5b37471a6763f2a069243e3c
vberkoz <vberkoz@vf-soft.com>
2023-06-17 22:36:27 +0300

    Update layout color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className="flex flex-col h-full">
+      <body className="flex flex-col h-full bg-[#EFEFEF]">
         <Header />
         {children}
         <Footer />
