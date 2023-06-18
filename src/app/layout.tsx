aa6bd0472246e38479e02eb0fa2c563b0e384b80
vberkoz <vberkoz@vf-soft.com>
2023-06-18 12:36:59 +0300

    Add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,22 @@ import "../styles/globals.css";
 import Footer from "./Footer";
 import Header from "./Header";
 
+import { IBM_Plex_Sans } from "next/font/google";
+const sans = IBM_Plex_Sans({
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+  variable: "--font-sans",
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+});
+
 export const metadata = {
-  title: "ThinkCar | Дистриб'юторська компанія продукції для діагностики авто",
-  description: "Дистриб'юторська компанія продукції для діагностики авто",
+  title: "ThinkCar | Distribution company of products for car diagnostics",
+  description: "Distribution company of products for car diagnostics",
+  icons: {
+    icon: '/icon.svg',
+    shortcut: '/icon.svg',
+    apple: '/icon.svg',
+  },
 };
 
 export default function RootLayout({
@@ -13,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className={`h-full ${sans.variable}`}>
       <body className="flex flex-col h-full bg-[#EFEFEF]">
         <Header />
         {children}
