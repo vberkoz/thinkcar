0e5fcd2996ef41207c2fee8a6c226d504263a804
vberkoz <vberkoz@vf-soft.com>
2023-06-18 12:38:17 +0300

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex-1">
-      <div className="container mx-auto">Головна</div>
+      <div className="container mx-auto">Home</div>
     </main>
   );
 }
