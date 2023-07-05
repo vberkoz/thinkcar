550dc47d6b994c4ef15503532ce326f50cac9e97
vberkoz <vberkoz@gmail.com>
2023-07-05 19:13:33 +0300

    Add Top products section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,19 @@ import slideThinktoolSe from "@/images/slide-thinktool-se.webp";
 import slideTpms from "@/images/slide-tpms.webp";
 import Slides from "./Slides";
 
-import cardImg1 from "@/images/thinktool-master-2.webp"
-import cardImg2 from "@/images/thinktool-master-x.webp"
-import cardImg3 from "@/images/thinktool.webp"
-import cardImg4 from "@/images/thinkcar-adas.webp"
-import cardImg5 from "@/images/thinkcar-prog.webp"
-import cardImg6 from "@/images/thinktool-lite.webp"
+import cardImg1 from "@/images/thinktool-master-2.webp";
+import cardImg2 from "@/images/thinktool-master-x.webp";
+import cardImg3 from "@/images/thinktool.webp";
+import cardImg4 from "@/images/thinkcar-adas.webp";
+import cardImg5 from "@/images/thinkcar-prog.webp";
+import cardImg6 from "@/images/thinktool-lite.webp";
+
+import thinkcarTWand900 from "@/images/thinkcarTWand900.webp";
+import thinkcarTWand100 from "@/images/thinkcarTWand100.webp";
+import thinktoolX10 from "@/images/thinktoolX10.webp";
+import thinktoolSE from "@/images/thinktoolSE.webp";
+import thinkcarTWand200 from "@/images/thinkcarTWand200.webp";
+import thinkcarProg from "@/images/thinkcarProg.webp";
 import SectionCTTTx2 from "./SectionCTTTx2";
 
 const slides = [
@@ -54,7 +61,7 @@ const novelty = {
       title: "THINKTOOL MASTER / Master 2",
       image: cardImg1.src,
       content: [
-        'Programming + Advanced coding + Technical support + Bidirectional control.',
+        "Programming + Advanced coding + Technical support + Bidirectional control.",
       ],
       link: "Learn more",
       href: "/",
@@ -105,13 +112,75 @@ const novelty = {
       href: "/",
     },
   ],
-}
+};
+
+const topProducts = {
+  id: "SectionCTTTx2",
+  caption: ["Top products"],
+  data: [
+    {
+      title: "THINKCAR T-Wand 900",
+      image: thinkcarTWand900.src,
+      content: [
+        "Complete vehicle diagnostics with intelligent TPMS sensor activation, programming, and relearning function.",
+      ],
+      link: "Learn more",
+      href: "/",
+    },
+    {
+      title: "THINKCAR T-Wand 100",
+      image: thinkcarTWand100.src,
+      content: [
+        "Full activation of TPMS sensor, programming function, and relearning.",
+      ],
+      link: "Learn more",
+      href: "/",
+    },
+    {
+      title: "THINKTOOL X10",
+      image: thinktoolX10.src,
+      content: [
+        "Remote video diagnostics tool.",
+      ],
+      link: "Learn more",
+      href: "/",
+    },
+    {
+      title: "THINKTOOL SE",
+      image: thinktoolSE.src,
+      content: [
+        "The latest 8-inch professional diagnostic tool with the latest Android 10 operating system.",
+      ],
+      link: "Learn more",
+      href: "/",
+    },
+    {
+      title: "THINKCAR T-Wand 200",
+      image: thinkcarTWand200.src,
+      content: [
+        "Integrated activation function, programming, and sensor TPMS relearning.",
+      ],
+      link: "Learn more",
+      href: "/",
+    },
+    {
+      title: "THINKCAR PROG",
+      image: thinkcarProg.src,
+      content: [
+        "Supports transmission programming for restoring old data or replacing the old transmission for recording new data.",
+      ],
+      link: "Learn more",
+      href: "/",
+    },
+  ],
+};
 
 export default function Home() {
   return (
     <main className="flex-1">
       <Slides data={slides} />
       <SectionCTTTx2 section={novelty} />
+      <SectionCTTTx2 section={topProducts} />
     </main>
   );
 }
